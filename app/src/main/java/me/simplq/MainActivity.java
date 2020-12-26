321cc1fd89b97acdb533b34fd2bb757411b60b5c
Dalton Fury <daltonfury42@gmail.com>
2020-12-26 19:19:40 +0530

    Fix typo and nit

--- app/src/main/java/me/simplq/MainActivity.java
+++ app/src/main/java/me/simplq/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLayout = findViewById(android.R.id.content);;
+        mLayout = findViewById(android.R.id.content);
 
         btnSendSMS = (Button) findViewById(R.id.btnSendSMS);
         txtPhoneNo = (EditText) findViewById(R.id.txtPhoneNo);
