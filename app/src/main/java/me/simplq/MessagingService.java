e69f561300af6a2c1cd3fda11845976e1d3121a2
Dalton Fury <daltonfury42@gmail.com>
2021-03-29 01:18:05 +0530

    Toast to give feedback on SMS

--- app/src/main/java/me/simplq/MessagingService.java
+++ app/src/main/java/me/simplq/MessagingService.java
@@ -1,12 +1,19 @@
 package me.simplq;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.telephony.SmsManager;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -41,8 +48,70 @@ public class MessagingService extends FirebaseMessagingService {
         }
     }
 
+    /*
+     * BroadcastReceiver mBrSend; BroadcastReceiver mBrReceive;
+     */
     private void sendSMS(String phoneNumber, String message) {
-        // TODO log/notify on failure
-        smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+        Context mContext = getApplicationContext();
+        ArrayList<PendingIntent> sentPendingIntents = new ArrayList<PendingIntent>();
+        ArrayList<PendingIntent> deliveredPendingIntents = new ArrayList<PendingIntent>();
+        PendingIntent sentPI = PendingIntent.getBroadcast(mContext, 0,
+                new Intent(mContext, SmsSentReceiver.class), 0);
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(mContext, 0,
+                new Intent(mContext, SmsDeliveredReceiver.class), 0);
+        try {
+            SmsManager sms = SmsManager.getDefault();
+            ArrayList<String> mSMSMessage = sms.divideMessage(message);
+            for (int i = 0; i < mSMSMessage.size(); i++) {
+                sentPendingIntents.add(i, sentPI);
+                deliveredPendingIntents.add(i, deliveredPI);
+            }
+            sms.sendMultipartTextMessage(phoneNumber, null, mSMSMessage,
+                    sentPendingIntents, deliveredPendingIntents);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), "SMS sending failed...", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public static class SmsDeliveredReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent arg1) {
+            switch (getResultCode()) {
+                case Activity.RESULT_OK:
+                    Toast.makeText(context, "SMS delivered", Toast.LENGTH_SHORT).show();
+                    break;
+                case Activity.RESULT_CANCELED:
+                    Toast.makeText(context, "SMS not delivered", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
+
+    public static class SmsSentReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent arg1) {
+            switch (getResultCode()) {
+                case Activity.RESULT_OK:
+                    Toast.makeText(context, "SMS Sent", Toast.LENGTH_SHORT).show();
+                    break;
+                case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                    Toast.makeText(context, "SMS generic failure", Toast.LENGTH_SHORT).show();
+                    break;
+                case SmsManager.RESULT_ERROR_NO_SERVICE:
+                    Toast.makeText(context, "SMS no service", Toast.LENGTH_SHORT)
+                            .show();
+                    break;
+                case SmsManager.RESULT_ERROR_NULL_PDU:
+                    Toast.makeText(context, "SMS null PDU", Toast.LENGTH_SHORT).show();
+                    break;
+                case SmsManager.RESULT_ERROR_RADIO_OFF:
+                    Toast.makeText(context, "SMS radio off", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
     }
 }
